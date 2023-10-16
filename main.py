8854c09bf958b4ba3326a96954b4e9752f31d930
samirametin <115466868+samirametin@users.noreply.github.com>
2023-10-16 23:03:55 +0400

    changed returns

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ def is_leap(year):
   if year % 4 == 0:
     if year % 100 == 0:
       if year % 400 == 0:
-        return "Leap year"
+        return True
       else:
-        return "Not leap year"
+        return False
     else:
-      return "Leap year"
+      return True
   else:
-    return "Not leap year"
+    return False
