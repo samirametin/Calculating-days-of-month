b8494fd64d0fd421abee9ed8f2838e59c1a142c4
samirametin <115466868+samirametin@users.noreply.github.com>
2023-10-16 23:06:51 +0400

    Finish

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def days_in_month(year, month):
     return 29
   return month_days[month-1]
 
-year = int(input()) # Enter a year
-month = int(input()) # Enter a month
+year = int(input("Enter a year"))
+month = int(input("Enter a month"))
 days = days_in_month(year, month)
 print(days)
