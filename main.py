ad33f40e3917049b5b8fefccbcde84629845d444
samirametin <115466868+samirametin@users.noreply.github.com>
2023-10-16 23:06:00 +0400

    calling functions and getting inputs

--- main.py
+++ main.py
@@ -16,3 +16,8 @@ def days_in_month(year, month):
   if years_condition and month == 2:
     return 29
   return month_days[month-1]
+
+year = int(input()) # Enter a year
+month = int(input()) # Enter a month
+days = days_in_month(year, month)
+print(days)
